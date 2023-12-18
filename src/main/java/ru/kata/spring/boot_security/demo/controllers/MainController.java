aba99bd658c62810561c24db9818a320367845a8
Danill Yunusov <dr-0901@mail.ru>
2023-12-18 15:37:38 +0300

    Correction

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/MainController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/MainController.java
@@ -1,17 +1,37 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public MainController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
     @GetMapping("/admin")
-    public String showAdminPage() {
+    public String showUsers(Model model, Principal principal) {
+        model.addAttribute("users", userService.getUsers());
+        model.addAttribute("currentUser", userService.findByName(principal.getName()));
+        model.addAttribute("roles", roleService.getAllRoles());
         return "admin";
     }
 
     @GetMapping("/user")
-    public String userInfo() {
+    public String homePage(Principal principal, Model model) {
+        User user = userService.findByName(principal.getName());
+        model.addAttribute("user", user);
         return "user";
     }
 
